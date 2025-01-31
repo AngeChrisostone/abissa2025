948d6bddb3472433cca3c66395a2683d08cf5261
Adnane Boukari <adnaneboukari665942@gmail.com>
2025-01-31 17:19:28 -0500

    Correction du header et footer S4

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,36 +1,33 @@
 import React from "react";
 import Image from "next/image";
 import logo from '@/public/img/logo_transparent.webp';
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io5";
 
-export default function Footer () {
+export default function Footer() {
   return (
-    <footer className=" bg-customcolor1 text-white p-4 text-[15px]  w-full">
-      <div className="text-right mr-20">Rejoignez-nous!</div>
-      <div className="flex flex-col md:flex-row justify-between items-center">
-        <Image
-          src={logo}
-          alt='Logo React'
-          width={100}
-        />
-        <div className="mt-4 md:mt-0">
+    <footer className="bg-customcolor1 text-white p-4 text-[15px] w-full">
+      <div className="flex flex-col md:flex-row justify-between items-center w-full">
+        <div className="mb-4 md:mb-0">
+          <Image
+            src={logo}
+            alt='Logo React'
+            width={100}
+          />
+        </div>
+        <div className="text-center flex-1 mb-4 md:mb-0">
           Contactez-nous :{" "}
           <a href="mailto:Abissa25@gmail.com" className="text-yellow-400 hover:underline">
             Abissa25@gmail.com
           </a>{" "}
           | Téléphone : 343 576 1024
         </div>
-        <div className="flex flex-col md:flex-row justify-between items-center p-4 gap-y-4 md:gap-x-8 mt-4 md:mt-0">
-          <div className="flex flex-col items-center">
-            <FaFacebook size={30} /> Facebook
-          </div>
-          <div className="flex flex-col items-center">
-            <FaInstagram size={30} /> Instagram
-          </div>
-          <div className="flex flex-col items-center">
-            <IoLogoWhatsapp size={30} /> Whatsapp
+        <div className="flex flex-col items-center">
+          <div className="text-center mb-2">Rejoignez-nous!</div>
+          <div className="flex space-x-4">
+            <a href="#Facebook" className="hover:underline flex items-center"><FaFacebook size={30} /> Facebook</a>
+            <a href="#Whatsapp" className="hover:underline flex items-center"><IoLogoWhatsapp size={30} /> Whatsapp</a>
+            <a href="#Instagram" className="hover:underline flex items-center"><FaInstagram size={30} /> Instagram</a>
           </div>
         </div>
       </div>
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
-export default function Navbar() {
-  return (
-    <ul className="hidden md:flex flex-1 justify-center gap-x-16 text-[25px] ">
-      <li><a href="#accueil" className="hover:underline">Accueil</a></li>
-      <li><a href="#histoire" className="hover:underline">Histoire</a></li>
-      <li><a href="#photos" className="hover:underline">Galerie</a></li>
-      <li><a href="#histoire" className="hover:underline">Programme</a></li>
-      <li><a href="#contact" className="hover:underline">Contact</a></li>
-    </ul>
-  );
+export default function Navbar({ changePage, vertical }) {
+    return (
+        <ul className={`flex ${vertical ? 'flex-col space-y-2' : 'flex-row space-x-4'}`}>
+            <li><button onClick={() => changePage('accueil')}>Accueil</button></li>
+            <li><button onClick={() => changePage('histoire')}>Histoire</button></li>
+            <li><button onClick={() => changePage('galerie')}>Galerie</button></li>
+            <li><button onClick={() => changePage('programme')}>Programme</button></li>
+            <li><button onClick={() => changePage('contact')}>Contact</button></li>
+        </ul>
+    );
 }

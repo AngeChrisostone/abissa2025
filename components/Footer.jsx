20cc8a4da66180d40973a101c493f94f30784094
Adnane Boukari <adnaneboukari665942@gmail.com>
2025-03-12 12:33:51 -0400

    Modifications mises a jours

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,32 +2,35 @@ import React from "react";
 import Image from "next/image";
 import logo from "@/public/img/logo_transparent.webp";
 import SocialLinks from "@/components/LienReseauSociaux";
+import { useTranslation } from "react-i18next";
 
 export default function Footer() {
+    const { t } = useTranslation("footer");
+
     return (
         <footer className="bg-customcolor1 text-white p-4 text-[15px] w-full">
             <div className="flex flex-col md:flex-row justify-between items-center w-full">
                 <div className="mb-4 md:mb-0">
                     <Image
                         src={logo}
-                        alt="Logo React"
+                        alt={t("logoAlt")}
                         width={100}
                         height={100}
                         style={{ objectFit: "cover" }}
                     />
                 </div>
                 <div className="text-center flex-1 mb-4 md:mb-0">
-                    Contactez-nous :{" "}
+                    {t("footer.email")}{" "}
                     <a
                         href="mailto:Abissa25@gmail.com"
                         className="text-yellow-400 hover:underline"
                     >
-                        Abissa25@gmail.com
+                        : Abissa25@gmail.com
                     </a>{" "}
-                    | Téléphone : 343 576 1024
+                    | {t("footer.phone")} : 343 576 1024
                 </div>
                 <div className="flex flex-col items-center">
-                    <div className="text-center mb-2">Rejoignez-nous!</div>
+                    <div className="text-center mb-2">{t("footer.contact")}</div>
                     <SocialLinks />
                 </div>
             </div>
